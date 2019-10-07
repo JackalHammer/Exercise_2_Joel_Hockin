9adb14a845729b411837af0a99761081c9418546
JackalHammer <46687793+JackalHammer@users.noreply.github.com>
2019-10-07 08:55:31 -0600

    Add files via upload

---
+++ Exercise_2_Joel_Hockin/js/js.js
@@ -0,0 +1,79 @@
+$('#flex1').hover(
+    function(){ $('#story1').removeClass('hidden')},
+    
+    function(){ $('#story1').addClass('hidden');
+});
+//       function(){ $(this).removeClass('hover') 
+
+
+$('#flex2').hover(
+       function(){ $('#story2').removeClass('hidden') },
+    
+    function(){ $('#story2').addClass('hidden');
+});
+
+$('#flex3').hover(
+       function(){ $('#story3').removeClass('hidden') },
+    
+    function(){ $('#story3').addClass('hidden');
+});
+
+$('#flex4').hover(
+       function(){ $('#story4').removeClass('hidden') },
+    
+    function(){ $('#story4').addClass('hidden');
+});
+
+$('#flex5').hover(
+       function(){ $('#story5').removeClass('hidden') },
+    
+    function(){ $('#story5').addClass('hidden');
+});
+
+$('#flex6').hover(
+       function(){ $('#story6').removeClass('hidden') },
+    
+    function(){ $('#story6').addClass('hidden');
+});
+
+
+
+//-----------------------------------------------
+
+$('#flex1').hover(
+    function(){ $('#flex1').removeClass('middle')},
+    
+    function(){ $('#flex1').addClass('middle');
+});
+//       function(){ $(this).removeClass('hover') 
+
+
+$('#flex2').hover(
+       function(){ $('#flex2').removeClass('middle') },
+    
+    function(){ $('#flex2').addClass('middle');
+});
+
+$('#flex3').hover(
+       function(){ $('#flex3').removeClass('middle') },
+    
+    function(){ $('#flex3').addClass('middle');
+});
+
+$('#flex4').hover(
+       function(){ $('#flex4').removeClass('middle') },
+    
+    function(){ $('#flex4').addClass('middle');
+});
+
+$('#flex5').hover(
+       function(){ $('#flex5').removeClass('middle') },
+    
+    function(){ $('#flex5').addClass('middle');
+});
+
+$('#flex6').hover(
+       function(){ $('#flex6').removeClass('middle') },
+    
+    function(){ $('#flex6').addClass('middle');
+});
